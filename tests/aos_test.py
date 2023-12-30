63b5cbba7dfd9c4c1fa0c627b7c8ada1d4a58474
Jakob Weichselbaumer <j.weichse@gmail.com>
2023-12-30 16:24:34 +0100

    Add AOS wrapper

---
+++ tests/aos_test.py
@@ -0,0 +1,25 @@
+import unittest
+import os
+from aos_wrapper import generate_integral
+
+class TestAOS(unittest.TestCase):
+    def test_aos_integrator(self):
+        batch_index = 0
+        required_file = f"../data/train/{batch_index}_0_pose_0_thermal.png"
+
+        no_train_error_msg = f"Please ensure you have set up training data using the preprocess.py and that the file {required_file} exists."
+        if not os.path.isdir("../data/train"):
+            raise FileNotFoundError(no_train_error_msg)
+        if not os.path.isfile(required_file):
+            raise FileNotFoundError(no_train_error_msg)
+
+        out_image = f"../data/train/{batch_index}_0-aos_thermal-0.png"
+        if os.path.isfile(out_image):
+            print("Deleting existing file.")
+            os.remove(out_image)
+        generate_integral(0, 0, _dir_adjust="..")
+
+        if not os.path.isfile(out_image):
+            raise FileNotFoundError("Couldn't find AOS output file, something went wrong.")
+
+

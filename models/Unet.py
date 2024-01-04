26fd3fb9811a6d06977cbc27278f99141c500a47
ciccio92kn <75566672+ciccio92kn@users.noreply.github.com>
2024-01-04 11:29:33 +0100

    Adapted JW changes

--- models/Unet.py
+++ models/Unet.py
@@ -80,6 +80,7 @@ class OutConv(nn.Module):
 class UNet(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=False):
         super(UNet, self).__init__()
+        self.model_name = "UNet"    #JW
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
@@ -95,6 +96,10 @@ class UNet(nn.Module):
         self.up3 = (Up(256, 128 // factor, bilinear))
         self.up4 = (Up(128, 64, bilinear))
         self.outc = (OutConv(64, n_classes))
+
+#JW implement
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        self.criterion = torch.nn.CrossEntropyLoss()
 
     def forward(self, x):
         x1 = self.inc(x)
